d0cdf93cf8179ef9673dcd3ea954ad0e9a41cc5d
Seimei Matsusaki <seimatsusaki@gmail.com>
2021-10-17 10:21:15 -0400

    Added rough skelton code for components.

--- src/components.tsx
+++ src/components.tsx
@@ -1,2 +1,41 @@
 import React from "react";
 
+function App () {
+    return (
+        <div></div>
+    )
+}
+
+function Header () {
+
+    return (
+        <nav></nav>
+    )
+}
+
+function Content () {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+function InputArea () {
+    return (
+        <div>
+            <textarea></textarea>
+        </div>
+    )
+}
+
+function OutputArea (props) {
+
+    const { tokens } = props; 
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
