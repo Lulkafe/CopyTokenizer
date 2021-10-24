388de0e4fe7fac18379e14e3ccd0f1a28509e265
Seimei Matsusaki <seimatsusaki@gmail.com>
2021-10-24 16:24:30 -0400

    Added rough skelton code of Setting window

--- src/components.tsx
+++ src/components.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useReducer, useContext, createContext } from "react";
+import { useReducer, useContext, createContext, useState } from "react";
 import { initState, TokenizerReducer, ACTION } from './reducer'
 import { linesToElements } from "./textTokenizer";
 
@@ -28,11 +28,56 @@ function Header () {
     return (
         <nav>
             <div id='nav__content-wrapper'>
-
+                {/* This Icon is temporary.
+                    Will replace with a setting icon later */}
+                <button type='button'>Icon</button>
+                <SettingMenu/>
             </div>
         </nav>
     )
 }
+
+function SettingMenu () {
+
+    return (
+        <div>
+            {/* TODO: This button element is temporary.
+                will replace with a setting icon later */}
+            <button type='button'>Open</button>
+            <div id='nav__setting-window'>
+                <ul>
+                    <li>Do you want to highlight a token when it is clicked?</li>
+                    <div>
+                        <input type='radio' name='token-clicked' id='nav__setting-radio-hl__yes' checked/>
+                        <label htmlFor='nav__setting-radio-hl'>Yes</label>
+                        <br/>
+                        <input type='radio' name='token-clicked' id='nav__setting-radio-hl__no'/>
+                        <label htmlFor='nav__setting-radio-hl'>No</label>
+                        <br/>
+                    </div>
+                    <li>Do you want to remove the highlight when you click the token again?</li>
+                    <div>
+                        <input type='radio' name='hl-clicked-again' id='nav__setting-radio-hlagain__yes' checked/>
+                        <label htmlFor='nav__setting-radio-hlagain'>Yes</label>
+                        <br/>
+                        <input type='radio' name='hl-clicked-again' id='nav__setting-radio-hlagain__no'/>
+                        <label htmlFor='nav__setting-radio-hlagain'>No</label>
+                        <br/>
+                    </div>
+
+                    <li>Enter characters you don't want to include in output tokens. (These will be converted into spaces)</li>
+                    <input type='text' placeholder='e.g. ,.-()[]'></input>
+
+                    <div>
+                        <button type='button'>Cancel</button>
+                        <button type='button'>OK</button>
+                    </div>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
 
 function Content (props) {
     return (
