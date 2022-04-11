6e6a5c274bd86302dd9c87a306cb842a05e6d8fa
Seimei Matsusaki <seimatsusaki@gmail.com>
2022-04-11 18:14:37 -0400

    Updated webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,16 +1,7 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const isProduction = process.env.NODE_ENV == 'production';
-
-
 const stylesHandler = 'style-loader';
-
-
-
-const config = {
+module.exports = {
     entry: './src/index.tsx',
     output: {
         path: path.resolve(__dirname, 'docs'),
@@ -20,9 +11,6 @@ const config = {
             favicon: './src/image/favicon.ico',
             template: './src/index.html',
         }),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
     module: {
         rules: [
@@ -39,9 +27,6 @@ const config = {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset'
             },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
         ],
     },
     resolve: {
@@ -49,13 +34,4 @@ const config = {
     },
 };
 
-module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
 
-    } else {
-        config.mode = 'development';
-    }
-    return config;
-};
